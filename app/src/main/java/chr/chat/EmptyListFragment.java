e93e7b97c19cb35f7904f9f8e6141cb86e50a18d
chervonyi <yuri.ternopil@gmail.com>
2019-05-08 13:08:59 -0500

    Add Main network of fragments

---
+++ app/src/main/java/chr/chat/EmptyListFragment.java
@@ -0,0 +1,35 @@
+package chr.chat;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
+
+public class EmptyListFragment extends Fragment {
+
+    private Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.chat_layout, container, false);
+        button = view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) Objects.requireNonNull(getActivity())).changeFragment(0, true);
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/chr/chat/MainActivity.java
+++ app/src/main/java/chr/chat/MainActivity.java
@@ -1,13 +1,51 @@
 package chr.chat;
 
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private List<Fragment> mFragments = new ArrayList<>();
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragments.add(new ChatFragment());
+        mFragments.add(new EmptyListFragment());
+
+        changeFragment(0, false);
+    }
+
+    @SuppressLint("ResourceType")
+    public void changeFragment(int position, boolean animation) {
+
+        // Remove current fragment if it has been added before
+        if (getSupportFragmentManager().findFragmentByTag(position + "") != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove(mFragments.get(position))
+                    .commit();
+        }
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        if (animation) {
+            fragmentTransaction.setCustomAnimations(R.anim.enter, 0);
+        }
+
+        // Add current fragment
+        fragmentTransaction.add(R.id.container, mFragments.get(position), position + "");
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
