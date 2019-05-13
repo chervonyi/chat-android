9c44b9a297967b3e133cb77b0f1b24725498e2d7
chervonyi <yuri.ternopil@gmail.com>
2019-05-13 18:13:34 -0500

    Add scrollbar to ChatList

--- app/src/main/java/chr/chat/fragments/HeaderChatListFragment.java
+++ app/src/main/java/chr/chat/fragments/HeaderChatListFragment.java
@@ -1,5 +1,6 @@
 package chr.chat.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,6 +17,7 @@ public class HeaderChatListFragment extends Fragment {
 
     private LinearLayout chatListContainer;
 
+    @SuppressLint("ResourceAsColor")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,11 +27,35 @@ public class HeaderChatListFragment extends Fragment {
         chatListContainer = view.findViewById(R.id.chat_list_container);
 
         ChatIconButton chatIconButton = new ChatIconButton(getContext());
+        chatIconButton.setBackgroundColor(R.color.red);
+        chatIconButton.setName("Yuri Chervonyi");
+        chatListContainer.addView(chatIconButton);
+
+
+        chatIconButton = new ChatIconButton(getContext());
+        chatIconButton.setBackgroundColor(R.color.blue);
+        chatIconButton.setName("Jon Snow");
         chatListContainer.addView(chatIconButton);
 
         chatIconButton = new ChatIconButton(getContext());
+        chatIconButton.setBackgroundColor(R.color.orange);
+        chatIconButton.setName("Dak Noise");
         chatListContainer.addView(chatIconButton);
 
+        chatIconButton = new ChatIconButton(getContext());
+        chatIconButton.setBackgroundColor(R.color.green);
+        chatIconButton.setName("Tark Morbund");
+        chatListContainer.addView(chatIconButton);
+
+        chatIconButton = new ChatIconButton(getContext());
+        chatIconButton.setBackgroundColor(R.color.violet);
+        chatIconButton.setName("Sansa Snow");
+        chatListContainer.addView(chatIconButton);
+
+        chatIconButton = new ChatIconButton(getContext());
+        chatIconButton.setBackgroundColor(R.color.yellow);
+        chatIconButton.setName("Khal Drogo");
+        chatListContainer.addView(chatIconButton);
 
         return view;
     }
--- app/src/main/java/chr/chat/views/ChatIconButton.java
+++ app/src/main/java/chr/chat/views/ChatIconButton.java
@@ -7,6 +7,8 @@ import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.util.Random;
+
 import chr.chat.R;
 
 @SuppressLint("AppCompatCustomView")
@@ -44,12 +46,43 @@ public class ChatIconButton extends Button {
         setLayoutParams(params);
 
 
-        setBackground(getResources().getDrawable(R.drawable.chat_icon_red));
+        //setBackground(getResources().getDrawable(R.drawable.chat_icon_red));
         setTextColor(getResources().getColor(R.color.white));
         setStateListAnimator(null);
         setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
         setText("YC");
 
         // TODO change text
+    }
+
+    public void setName(String name) {
+
+        String shortForm = "";
+
+        for (char c : name.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                shortForm += c;
+            }
+        }
+
+        if (shortForm.length() == 0) {
+            shortForm = String.valueOf(new Random().nextInt(98) + 1);
+        } else if (shortForm.length() > 2) {
+            shortForm = shortForm.substring(0, 2);
+        }
+
+        setText(shortForm);
+    }
+
+    public void setBackgroundColor(int color) {
+        switch (color) {
+            case R.color.blue:      setBackgroundResource(R.drawable.chat_icon_blue);   break;
+            case R.color.yellow:    setBackgroundResource(R.drawable.chat_icon_yellow); break;
+            case R.color.red:       setBackgroundResource(R.drawable.chat_icon_red);    break;
+            case R.color.violet:    setBackgroundResource(R.drawable.chat_icon_violet); break;
+            case R.color.green:     setBackgroundResource(R.drawable.chat_icon_green);  break;
+            case R.color.orange:    setBackgroundResource(R.drawable.chat_icon_orange); break;
+            default:                setBackgroundResource(R.drawable.chat_icon_red);    break;
+        }
     }
 }
